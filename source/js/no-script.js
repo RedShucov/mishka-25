d5759ffe9de1055b60ccdbd95d21dc78c6e79c46
Vladimir Polozov <65243518+RedShucov@users.noreply.github.com>
2022-05-13 18:10:51 +0300

    Добавлены стили и скрипты для хэдера

---
+++ source/js/no-script.js
@@ -0,0 +1,6 @@
+const navigationNoJs = document.querySelector('.navigation')
+const namburgerNoJs = document.querySelector('.header__hamburger')
+
+navigationNoJs.classList.remove('navigation--no-js')
+
+namburgerNoJs.classList.remove('header__hamburger--no-js')
