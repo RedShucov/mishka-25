9257d895fb000d07a41fad51b91d26383943f71c
Vladimir Polozov <65243518+RedShucov@users.noreply.github.com>
2022-05-16 11:12:13 +0300

    Удалена папка source

--- source/js/no-script.js
+++
@@ -1,6 +0,0 @@
-const navigationNoJs = document.querySelector('.navigation')
-const namburgerNoJs = document.querySelector('.header__hamburger')
-
-navigationNoJs.classList.remove('navigation--no-js')
-
-namburgerNoJs.classList.remove('header__hamburger--no-js')
